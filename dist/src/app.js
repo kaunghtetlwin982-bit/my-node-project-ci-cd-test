075d886ed1cd135ac7ea9604ebb0a25cbb2c474e
kaunghtetlwin982-bit <kaunghtetlwin@gmail.com>
2026-04-05 23:42:22 +0630

    update of run project

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,8 +1,6 @@
 import express from 'express';
-import { connectDb } from './ helper/dbHelper.js';
 const app = express();
 const port = 3005;
-connectDb();
 app.get('/', (req, res) => {
     res.json({ message: "Hello TypeScript Node.js CI/CD! to test" });
 });
